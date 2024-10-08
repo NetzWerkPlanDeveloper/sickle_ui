cf5d75f7ae86cf34fb3d1432d001a8b84792b3e7
Oliver Kolligs <kolligs@netzwerkplan.de>
2024-10-08 18:03:38 +0200

    try to use windows delimiters

--- src/assets/fonts/mod.rs
+++ src/assets/fonts/mod.rs
@@ -1,21 +1,19 @@
 use bevy::{asset::embedded_asset, prelude::*};
-use std::path::Path;
 
 pub(crate) struct BuiltInFontsPlugin;
 
 impl Plugin for BuiltInFontsPlugin {
     fn build(&self, app: &mut App) {
-        let path = Path::new("src/assets");
-        embedded_asset!(app, path, "FiraSans-Bold.ttf");
-        embedded_asset!(app, path, "FiraSans-BoldItalic.ttf");
-        embedded_asset!(app, path, "FiraSans-Italic.ttf");
-        embedded_asset!(app, path, "FiraSans-Medium.ttf");
-        embedded_asset!(app, path, "FiraSans-MediumItalic.ttf");
-        embedded_asset!(app, path, "FiraSans-Regular.ttf");
-        embedded_asset!(app, path, "FiraSansCondensed-Bold.ttf");
-        embedded_asset!(app, path, "FiraSansCondensed-BoldItalic.ttf");
-        embedded_asset!(app, path, "FiraSansCondensed-Italic.ttf");
-        embedded_asset!(app, path, "FiraSansCondensed-Regular.ttf");
-        embedded_asset!(app, path, "MaterialIcons-Regular.ttf");
+        embedded_asset!(app, "src\\assets\\", "FiraSans-Bold.ttf");
+        embedded_asset!(app, "src\\assets\\", "FiraSans-BoldItalic.ttf");
+        embedded_asset!(app, "src\\assets\\", "FiraSans-Italic.ttf");
+        embedded_asset!(app, "src\\assets\\", "FiraSans-Medium.ttf");
+        embedded_asset!(app, "src\\assets\\", "FiraSans-MediumItalic.ttf");
+        embedded_asset!(app, "src\\assets\\", "FiraSans-Regular.ttf");
+        embedded_asset!(app, "src\\assets\\", "FiraSansCondensed-Bold.ttf");
+        embedded_asset!(app, "src\\assets\\", "FiraSansCondensed-BoldItalic.ttf");
+        embedded_asset!(app, "src\\assets\\", "FiraSansCondensed-Italic.ttf");
+        embedded_asset!(app, "src\\assets\\", "FiraSansCondensed-Regular.ttf");
+        embedded_asset!(app, "src\\assets\\", "MaterialIcons-Regular.ttf");
     }
 }
